9b7fd9c33ef87bcb6626dcef43ca3112012d9c2c
unknown <anderqui001@warren.k12.in.us>
2018-12-14 13:30:32 -0500

    pictures

--- motor.js
+++ motor.js
@@ -3,3 +3,11 @@ $(document).ready(function(){
         $("#panel").slideToggle("slow");
     });
 });
+
+$(document).ready(function(){
+    $("h2").click(function(){
+      $("#div1").fadeOut(2000);
+      $("#div2").fadeOut(8000);
+      $("#div3").fadeOut(16000);
+    });
+  });
